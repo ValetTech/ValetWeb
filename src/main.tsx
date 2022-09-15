8465d18e8f02fec942bbf3e1d18d78e38b4f54c1
paulpd1 <paul.boaden1@studytafensw.edu.au>
2022-09-15 13:41:38 +1000

    Resolved import bug and deleted store.

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,9 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import App from './App';
 import './index.css';
-import { store } from './Store/store';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <App />
   </React.StrictMode>
 );
