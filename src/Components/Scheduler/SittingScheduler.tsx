b271a744eb930e89b777c3bdba03920aa9d47b17
paulboaden <paulboaden1@outlook.com>
2022-10-24 18:48:58 +1100

    Add fetch to calendar

--- src/Components/Scheduler/SittingScheduler.tsx
+++ src/Components/Scheduler/SittingScheduler.tsx
@@ -1,12 +1,52 @@
+// Components
+// #region
 import '@fullcalendar/react/dist/vdom';
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import interactionPlugin from '@fullcalendar/interaction';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { v4 as uuidv4 } from 'uuid';
+// #endregion
+
+// Services
+// #region
+import { getSittingsAsync } from '../../Services/ApiServices';
+// #endregion
+
+// Models
+// #region
+import Sitting from '../../Models/Sitting';
+// #endregion
 
 export default function SittingScheduler() {
+  const [sittingData, setSittingData] = useState([]);
   const [events, setEvents] = useState([]);
+
+  useEffect(() => {
+    function createEvents() {
+      const sittings: { type: string; startTime: string; endTime: string }[] =
+        sittingData.map((s) => ({
+          title: s.type,
+          start: s.startTime,
+          end: s.endTime,
+        }));
+      setEvents(sittings);
+    }
+    async function fetchSittings() {
+      const res: Sitting[] = await getSittingsAsync();
+      setSittingData(res);
+    }
+    fetchSittings().then(() => {
+      createEvents();
+    });
+  }, []);
+
+  // const sittings: { name: string; startTime: string; endTime: string }[] =
+  //   sittingData.map((s) => ({
+  //     title: s.name,
+  //     start: s.startTime,
+  //     end: s.endTime,
+  //   }));
 
   const handleSelect = (info: { start: any; end: any }) => {
     const { start, end } = info;
