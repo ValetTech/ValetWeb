aacabaa5209a21a402fedfd6983b85005e0855e9
Jamie Creighton <jaijai.kc@gmail.com>
2022-11-04 12:56:55 +1100

    Fix redux import

--- src/Redux/store.ts
+++ src/Redux/store.ts
@@ -1,17 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
-import { authApi } from './api/authApi';
-import { userApi } from './api/userApi';
-import userReducer from './userSlice';
+// import { authApi } from './api/authApi';
+// import { userApi } from './api/userApi';
 import authReducer from './authSlice';
+import userReducer from './userSlice';
 
 // eslint-disable-next-line import/prefer-default-export
 export const store = configureStore({
   reducer: {
     // ? Add the authReducer to the reducer object
     authUser: authReducer,
-    [authApi.reducerPath]: authApi.reducer,
-    [userApi.reducerPath]: userApi.reducer,
+    // [authApi.reducerPath]: authApi.reducer,
+    // [userApi.reducerPath]: userApi.reducer,
     userState: userReducer,
   },
   devTools: process.env.NODE_ENV !== 'development',
