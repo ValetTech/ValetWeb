d5f5b1511fd8870d8a6f948b108364075f3243e0
paulboaden <paulboaden1@outlook.com>
2022-11-13 19:14:04 +1100

    Make default colour scheme dark

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ import DefaultAppShell from './Layouts/AppShell';
 export default function App() {
   const [colorScheme, setColorScheme] = useLocalStorage<ColorScheme>({
     key: 'mantine-color-scheme',
-    defaultValue: 'light',
+    defaultValue: 'dark',
     getInitialValueInEffect: true,
   });
 
