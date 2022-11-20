64eee424240a0e62972119bcd96935c26b5f27e7
paulboaden <paulboaden1@outlook.com>
2022-11-20 16:08:27 +1100

    Add localstorage

--- src/Features/Auth/authSlice.ts
+++ src/Features/Auth/authSlice.ts
@@ -2,15 +2,30 @@
 /* eslint-disable @typescript-eslint/no-unused-expressions */
 /* eslint-disable no-param-reassign */
 import { createSlice } from '@reduxjs/toolkit';
+import loadState from '../LocalStorage/localStorage';
+
+function saveState(state: any){
+  try {
+    const serializedState = JSON.stringify(state);
+    localStorage.setItem('state', serializedState);
+  } catch {
+    // ignore write errors
+  }
+};
+
+const localUser = loadState();
+console.log(localUser);
 
 const authSlice = createSlice({
   name: 'auth',
-  initialState: { user: null, token: null },
+  initialState: { user: localUser.user, token: localUser.token },
   reducers: {
     setCredentials: (state, action) => {
       const { user, accessToken } = action.payload
       state.user = user
       state.token = accessToken
+
+      saveState(state);
     },
     logOut: (state, action) => {
       state.user = null
